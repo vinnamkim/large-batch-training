75ba057ff677980edfcbba3498fe9213a6bd2710
Vinnam Kim <vinnam.kim@gmail.com>
2019-11-06 00:18:18 +0900

    Update plot_parametric_pytorch.py

--- PyTorch/plot_parametric_pytorch.py
+++ PyTorch/plot_parametric_pytorch.py
@@ -27,16 +27,19 @@ import torch
 torch.manual_seed(1337)
 from torch.autograd import Variable
 import torch.nn.functional as F
-import keras #This dependency is only for loading the CIFAR-10 data set
-from keras.datasets import cifar10
+#import keras #This dependency is only for loading the CIFAR-10 data set
 from copy import deepcopy
 import vgg 
 
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
+import torchvision
+import torch
+from torchvision import transforms
+
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
 # This is where you can load any model of your choice.
 # I stole PyTorch Vision's VGG network and modified it to work on CIFAR-10.
@@ -59,44 +62,50 @@ x0 = deepcopy(model.state_dict())
 
 # Number of epochs to train for
 # Choose a large value since LB training needs higher values
-nb_epochs = 150
+nb_epochs = 100
 
 
 # If SB.pth and LB.pth are available
 # set hotstart = True and run only the
 # parametric plot (i.e., don't train the network)
-hotstart = False
+hotstart = True
+
+criterion = torch.nn.CrossEntropyLoss()
 
 if not hotstart:
-    for fractions_of_dataset in [10, 200]: #Run with 1/10th the data set and 1/200th the dataset
+    for fractions_of_dataset in [50, 200]: #Run with 1/10th the data set and 1/200th the dataset
         optimizer = torch.optim.Adam(model.parameters())
         model.load_state_dict(x0)
+        model.cuda()
         average_loss_over_epoch = '-'
-        print 'Optimizing the network with batch size %d' % (X_train.shape[0]/fractions_of_dataset)
+        batch_size = int(len(trainset)/fractions_of_dataset)
+        print('Optimizing the network with batch size {0}'.format(batch_size))
+        
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+        
         np.random.seed(1337) #So that both networks see same sequence of batches
         for e in range(nb_epochs):
             model.eval()
-            print 'Epoch:', e, ' of ', nb_epochs, 'Average loss:', average_loss_over_epoch
+            print('Epoch:', e, ' of ', nb_epochs, 'Average loss:', average_loss_over_epoch)
             average_loss_over_epoch = 0.
             # Checkpoint the model every epoch
             torch.save(model.state_dict(), ('SB' if fractions_of_dataset==200 else 'LB')+'.pth')
 
             # Training loop!
-            for smpl in np.split(np.random.permutation(range(X_train.shape[0])), fractions_of_dataset):
+            for images, labels in trainloader:
                 model.train()
                 optimizer.zero_grad()
-                ops = opfun(X_train[smpl])
-                tgts = Variable(torch.from_numpy(y_train[smpl]).long().squeeze())
-                loss_fn = F.nll_loss(ops, tgts)
-                average_loss_over_epoch += loss_fn.data.numpy()[0] / fractions_of_dataset
-                loss_fn.backward()
+                ops = model(images.cuda())
+                loss = criterion(ops, labels.cuda())
+                average_loss_over_epoch += loss / float(fractions_of_dataset)
+                loss.backward()
                 optimizer.step()
 
 # Load stored values
 # If hotstarted, loop is ignored and SB/LB files must be provided
 mbatch = torch.load('LB.pth')
 mstoch = torch.load('SB.pth')
-print 'Loaded stored solutions'
+print('Loaded stored solutions') 
 
 
 grid_size = 25 #How many points of interpolation between [-1, 2]
@@ -106,24 +115,53 @@ i = 0
 
 # Fill in the train and test, loss and accuracy values
 # for `grid_size' points in the interpolation
-for alpha in alpha_range:
+for i, alpha in enumerate(alpha_range):
     mydict = {}
-    for key, value in mbatch.iteritems():
-        mydict[key] = value * alpha + (1 - alpha) * mstoch[key]
+    for key in mbatch:
+        if 'var' in key:
+            x = mbatch[key].sqrt() * alpha + (1 - alpha) * mstoch[key].sqrt()
+            mydict[key] = x * x
+        else:
+            mydict[key] = mbatch[key] * alpha + (1 - alpha) * mstoch[key]
     model.load_state_dict(mydict)
+    model.cuda()
     testloss = trainloss = testacc = trainacc = 0.
-    j = 0
-    for datatype in [(X_train, y_train), (X_test, y_test)]:
-        dataX = datatype[0]
-        datay = datatype[1]
-        for smpl in np.split(np.random.permutation(range(dataX.shape[0])), 10):
-            ops = opfun(dataX[smpl])
-            tgts = Variable(torch.from_numpy(datay[smpl]).long().squeeze())
-            data_for_plotting[i, j] += F.nll_loss(ops, tgts).data.numpy()[0] / 10.
-            data_for_plotting[i, j+2] += accfun(ops, datay[smpl]) / 10.
-        j += 1
-    print data_for_plotting[i]
-    i += 1
+    batch_size = 5000
+    
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=True, num_workers=2)
+    
+    model.eval()
+    with torch.no_grad():
+        total = 0
+        correct = 0
+        for images, labels in trainloader:
+            outputs = model(images.cuda())
+            loss = criterion(outputs, labels.cuda())
+            trainloss += loss / float(len(trainset) / batch_size)
+            _, predicted = torch.max(outputs.cpu(), 1)
+            total += labels.size(0)
+            correct += (predicted.cpu() == labels).sum().item()
+        trainacc = correct / float(total)
+
+        total = 0
+        correct = 0
+        for images, labels in testloader:
+            outputs = model(images.cuda())
+            loss = criterion(outputs, labels.cuda())
+            testloss += loss / float(len(testset) / batch_size)
+            _, predicted = torch.max(outputs.cpu(), 1)
+            total += labels.size(0)
+            correct += (predicted.cpu() == labels).sum().item()
+        testacc = correct / float(total)
+    
+    data_for_plotting[i, 0] = trainloss
+    data_for_plotting[i, 1] = testloss
+    data_for_plotting[i, 2] = trainacc
+    data_for_plotting[i, 3] = testacc
+    
+    print('alpha : {0} done'.format(alpha))
+
 np.save('intermediate-values', data_for_plotting)
 
 # Actual plotting;
@@ -145,4 +183,4 @@ ax1.legend(('Train', 'Test'), loc=0)
 
 ax1.grid(b=True, which='both')
 plt.savefig('C3ish.pdf')
-print 'Saved figure; Task complete'
+print('Saved figure; Task complete')
